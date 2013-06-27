f9383c0b928fc4d9aeaaf6cec2e2e549158c67a0
yize <yize.shc@gmail.com>
2013-06-27 23:40:27 +0800

    fix node 0.8

--- test/html_minify_test.js
+++ test/html_minify_test.js
@@ -8,8 +8,8 @@ module.exports = {
     normal: function (test) {
         test.expect(1);
 
-        var actual = fs.readFileSync('test/fixtures/index.html').toString('utf8');
-        var expected = fs.readFileSync('test/expected/index.html').toString('utf8');
+        var actual = fs.readFileSync('test/fixtures/index.html','utf8').toString();
+        var expected = fs.readFileSync('test/expected/index.html','utf8').toString();
         test.equal(actual, expected, 'should describe what the default behavior is.');
 
         test.done();
@@ -18,8 +18,8 @@ module.exports = {
     gbk: function (test) {
         test.expect(1);
 
-        var actual = fs.readFileSync('test/fixtures/index-gbk.html').toString('utf8');
-        var expected = fs.readFileSync('test/expected/index-gbk.html').toString('utf8');
+        var actual = fs.readFileSync('test/fixtures/index-gbk.html','utf8').toString();
+        var expected = fs.readFileSync('test/expected/index-gbk.html','utf8').toString();
         test.equal(actual, expected, 'should describe what the default behavior is.');
 
         test.done();
