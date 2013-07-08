a14b09f8e7004826c213c2550c7de6d9385a7e1e
yize <yize.shc@gmail.com>
2013-07-08 21:18:42 +0800

    fixes #1

--- index.js
+++
@@ -1,9 +0,0 @@
-/*
- * author : yize <yize.shc@gmail.com>
- * date : 2013-05-30
- */
-"use strict";
-var minify = require('./lib/minify.js');
-module.exports=function(cont,callback){
-    minify(cont,callback);
-};
